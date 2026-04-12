c94a0c684dea9f67208df65eb3604312ae50df40
fauzannurhikmah <fauzannurhikmah0@gmail.com>
2026-04-13 01:14:23 +0700

    fix: streamline allowed origins in CORS middleware for improved security and clarity

--- app/main.py
+++ app/main.py
@@ -73,16 +73,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:8000",
-        "https://video-downloader-vert-five.vercel.app/",
-        os.getenv("FRONTEND_URL", os.getenv("FRONTEND_URL"))
+        "https://video-downloader-vert-five.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(router, prefix="/api")
 
